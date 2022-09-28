c2dd371f7d4780d1a37807901755ec1ad8811c43
lucasviinic <lvms@cin.ufpe.br>
2022-09-27 23:26:43 -0300

    exploring terminal commands

---
+++ fundamentals/commands/commands.go
@@ -0,0 +1,7 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Printf("Outro programa em %s!!!!\n", "Go") //Printf permite a passagem de valores para a string por meio de "ganchos" iniciados por %
+}
