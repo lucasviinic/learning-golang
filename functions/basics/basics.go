bd11126ea71197ffc461ba0d1411fc67cc7bebc4
lucasviinic <lvms@cin.ufpe.br>
2022-10-09 10:10:05 -0300

    basic functions

---
+++ functions/basics/basics.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+func f1() {
+	fmt.Println("F1")
+}
+
+func f2(p1 string, p2 string) {
+	fmt.Printf("F2: %s %s\n", p1, p2)
+}
+
+func f3() string {
+	return "F3"
+}
+
+func f4(p1, p2 string) string {
+	return fmt.Sprintf("F4: %s %s", p1, p2)
+}
+
+func f5() (string, string) {
+	return "Retorno1", "Retorno 2"
+}
+
+func main() {
+	f1()
+	f2("Param 1", "Param 2")
+
+	r3, r4 := f3(), f4("Param 1", "Param 2")
+	fmt.Printf("r3: %s r4: %s \n", r3, r4)
+
+	r51, r52 := f5()
+	fmt.Println("F5: ", r51, r52)
+}
