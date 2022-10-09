b7ff472b011993ec567b0749cbed925e4e5584e0
lucasviinic <lvms@cin.ufpe.br>
2022-10-09 11:16:56 -0300

    variatic functions

---
+++ functions/variatic/variatic.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func media(numeros ...float64) float64 {
+	total := 0.0
+	for _, num := range numeros {
+		total += num
+	}
+	return total / float64(len(numeros))
+}
+
+func main() {
+	fmt.Printf("Média: %.2f", media(23.4, 2, 42, -9, 3, -3.2, 75, -6.4))
+}
